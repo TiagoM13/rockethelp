7fec8b3ddbde6f2d0a80f10bf76f96092160261f
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-09 21:49:57 -0300

    add google fonts and load fonts in app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import SignIn from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
+
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
