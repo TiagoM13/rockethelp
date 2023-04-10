8c0d8ac15371c356d97b60b9edfce8190760a26d
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-10 20:44:46 -0300

    add svg types and settings and use them

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,13 @@
 import { Heading, VStack } from "native-base";
 import React from "react";
 
+import Logo from "../assets/logo_primary.svg";
+
 const screens = () => {
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
