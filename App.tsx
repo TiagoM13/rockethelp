6f2fdedd99885fc7c1493a6d82c733958bf65a87
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-09 21:54:30 -0300

    styling the status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
@@ -13,8 +13,15 @@ import { THEME } from "./src/styles/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
