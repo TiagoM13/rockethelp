64758d419de40ebda22542e7aecd9ad7a8c1eddf
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-27 22:20:05 -0300

    using firebase auth and user validation

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,8 @@
-import { Heading, Icon, VStack, useTheme } from "native-base";
-import { Envelope, Key } from "phosphor-react-native";
 import React, { useState } from "react";
+import { Alert } from "react-native";
+import { Heading, Icon, VStack, useTheme } from "native-base";
+import auth from "@react-native-firebase/auth";
+import { Envelope, Key } from "phosphor-react-native";
 
 import Logo from "@app/assets/logo_primary.svg";
 
@@ -8,13 +10,39 @@ import { Input } from "@app/components/Input";
 import { Button } from "@app/components/Button";
 
 const screens = () => {
-  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { colors } = useTheme();
 
   const handleSignIn = () => {
-    console.log(name, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe um e-mail e senha!");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválid0.");
+        }
+
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar");
+      });
   };
 
   return (
@@ -31,7 +59,7 @@ const screens = () => {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
 
       <Input
@@ -42,7 +70,12 @@ const screens = () => {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 };
