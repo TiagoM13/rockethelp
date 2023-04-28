a4ab23485a30d5330948a17c8ffc3e232cc10443
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-27 22:21:49 -0300

    add access to routes for authenticated user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
+import { Loading } from "@app/components/Loading";
 import SignIn from "@app/screens/SignIn";
 import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
+  const [loading, setLoandig] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setLoandig(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 };
